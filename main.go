72d83d933d3bcd0ce6afdc27bc7bebda57811dc7
Fin <shadowyeet23@gmail.com>
2026-08-28 19:23:21 +0200

    Added the levels api for levels.json in the backend

--- main.go
+++ main.go
@@ -17,6 +17,15 @@ func main() {
 		w.Header().Set("Content-type", "application/json")
 		w.Write(parts)
 	})
+	mux.HandleFunc("GET /api/levels", func(w http.ResponseWriter, r *http.Request) {
+		levels, err := os.ReadFile("levels.json")
+		if err != nil {
+			http.Error(w, "Could not read levels.json file", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write(levels)
+	})
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 
 	http.ListenAndServe(":8080", mux)
